ec09888e0d1fb7e608a38e270a4e4d849f491a6d
Henrique Figueiredo <henricklima94@gmail.com>
2021-10-16 00:04:01 -0300

    modify scriptLogin.js

--- public/assets/js/scriptLogin.js
+++ public/assets/js/scriptLogin.js
@@ -1,6 +1,6 @@
 /* Cancelar envio do formulário caso haja algum campo do formulário vazio */
-const fieldset = document.querySelector('.div-fieldset fieldset');
-const formLogin = fieldset.querySelector('form');
+const divConteudo = document.querySelector('.conteudo');
+const formLogin = divConteudo.querySelector('form');
 const loginUsuario = formLogin.querySelector('#usuario');
 const loginSenha = formLogin.querySelector('#senha');
 
@@ -12,10 +12,14 @@ formLogin.addEventListener('submit', function(event){
 })
 
 function criarAviso(){
+  if(divConteudo.querySelector('.alert')){
+    let $divAlert = divConteudo.querySelector('.alert');
+    divConteudo.removeChild($divAlert);
+  }
   const $div = document.createElement('div');
   const $text = document.createTextNode('Campo e-mail e/ou usuário vazios');
   $div.appendChild($text);
   $div.classList.add('alert');
   $div.classList.add('alert-danger');
-  fieldset.appendChild($div);
+  divConteudo.appendChild($div);
 }

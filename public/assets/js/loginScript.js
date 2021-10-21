42f49dfa00afe990ad14cb537f7a0524d59b816a
Henrique Figueiredo <henricklima94@gmail.com>
2021-10-21 19:48:35 -0300

    add changes in .ejs and .css files for mobile version

--- public/assets/js/loginScript.js
+++ public/assets/js/loginScript.js
@@ -3,23 +3,55 @@ const divConteudo = document.querySelector('.conteudo');
 const formLogin = divConteudo.querySelector('form');
 const loginUsuario = formLogin.querySelector('#usuario');
 const loginSenha = formLogin.querySelector('#senha');
+const divAlert  = divConteudo.querySelector('#alert-div');
 
 formLogin.addEventListener('submit', function(event){
   if(!loginUsuario.value || !loginSenha.value){
     event.preventDefault();
     criarAviso();
   }
-})
+});
 
 function criarAviso(){
-  if(divConteudo.querySelector('.alert')){
-    let $divAlert = divConteudo.querySelector('.alert');
-    divConteudo.removeChild($divAlert);
-  }
-  const $div = document.createElement('div');
+  verificarAlert();
+  /* const $div = document.createElement('div'); */
   const $text = document.createTextNode('Campo e-mail e/ou usuário vazios');
-  $div.appendChild($text);
-  $div.classList.add('alert');
-  $div.classList.add('alert-danger');
-  divConteudo.appendChild($div);
+  divAlert.appendChild($text);
+  divAlert.classList.add('alert');
+  divAlert.classList.add('alert-danger');
+  /* divConteudo.appendChild(divAlert); */
+}
+
+function verificarAlert(){
+  if(divConteudo.querySelector('.alert')){
+    divAlert.textContent = '';
+  }
+}
+
+/* Senha de Visitante */
+
+const fieldsetDivs = document.querySelectorAll('.div-fieldset-div');
+const btnVisitante = document.querySelector('#visitante');
+const btnVoltar = document.querySelector('#voltar');
+
+btnVisitante.addEventListener('click', function(){
+  active();
+  this.style.visibility = 'hidden';
+  verificarAlert();
+});
+
+btnVoltar.addEventListener('click', function(){
+  active();
+  btnVisitante.style.visibility = 'visible';
+  verificarAlert();
+});
+
+function active(){
+  fieldsetDivs.forEach(div =>{
+    if(div.className === 'div-fieldset-div active'){
+      div.classList.remove('active');
+    }
+    else
+    div.classList.add('active');
+  });
 }
